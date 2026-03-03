b1d3e4a9f9920bfc4a30a619401eff91ef10bc6c
unknown <dayren86@gmial.com>
2026-03-03 16:22:00 +0200

    add  docker

--- src/test/java/mate/academy/HwApplicationTests.java
+++ src/test/java/mate/academy/HwApplicationTests.java
@@ -1,13 +1,8 @@
 package mate.academy;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HwApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
